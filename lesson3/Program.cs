08f96646034a21d631d47acd0f14be4c488def6b
engyasir <yasser7491@yahoo.com>
2016-10-03 00:29:11 +0300

    Add project files.

---
+++ lesson3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char myvire_value =Convert.ToChar (99);
+           // int Y_value = 'Y';
+            Console.WriteLine("the value is = {0} ", myvire_value);
+            Console.ReadKey();
+        }
+    }
+}
